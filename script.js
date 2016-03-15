9e6ab5cc8846bd3199534bd91d93413e4d113388
ColinBeal <boblafriteusegames@gmail.com>
2016-03-15 21:02:56 +0100

    Ajout Majeur

---
+++ script.js
@@ -0,0 +1,22 @@
+function changeImage() {
+    var image = document.getElementById('mute');
+    if (image.src.match("sound")) {
+        image.src = "images/mute.png";
+    }
+    else {
+        image.src = "images/sound.png";
+    }
+}
+var audio = document.getElementById('audio');
+
+document.getElementById('mute').addEventListener('click', function (e)
+{
+    e = e || window.event;
+    audio.muted = !audio.muted;
+    e.preventDefault();
+
+}, false);
+function overlay() {
+    el = document.getElementById("overlay");
+    el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+}
